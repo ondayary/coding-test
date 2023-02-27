c3f3e299bdc38fdf9dde20d5765e757b8ab85185
이다온 <ondayary@gmail.com>
2023-02-27 22:37:57 +0900

    [기초-종합] 수 나열하기1

---
+++ src/codeup/basic100/N1090.java
@@ -0,0 +1,14 @@
+package codeup.basic100;
+
+import java.util.Scanner;
+
+public class N1090 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+    /**
+     * 입력:
+     * 출력:
+     */
+}
