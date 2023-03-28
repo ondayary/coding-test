3853b5241f2e92aa111f6f095b717345b4bbc570
이다온 <ondayary@gmail.com>
2023-03-28 14:56:28 +0900

    애너그램 만들기 - 리팩토링

--- src/baekjoon/part1/N1919.java
+++ src/baekjoon/part1/N1919.java
@@ -3,28 +3,36 @@ package baekjoon.part1;
 import java.util.Scanner;
 
 public class N1919 {
+    public static int[] get_alphabet_count(String str) {
+        int[] count = new int[26];
+
+        for(int i = 0; i < str.length(); i++)
+            count[str.charAt(i) - 'a']++; // 'a'를 빼줘야 인덱스 안에 들어온다.
+
+        return count;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next(); // 공백으로 구분된 한 문자열을 받기 위해서는 next를 쓰면 된다.
         String b = sc.next();
 
         // a와 b의 알파벳 구성을 구할 것이다.
-        int[] a_count = new int[26];
-        int[] b_count = new int[26];
-
-        for(int i = 0; i < a.length(); i++)
-            a_count[a.charAt(i) - 'a']++; // 'a'를 빼줘야 인덱스 안에 들어온다.
-        for(int i = 0; i < b.length(); i++)
-            b_count[b.charAt(i) - 'a']++;
-
+        int[] a_count = get_alphabet_count(a);
+        int[] b_count = get_alphabet_count(b);
 
         int answer = 0;
         for (int i = 0; i < 26; i++) {
             // 차이가 나는 부분들을 더해주면 된다.
+            // 단순히 빼게 되면 음수가 나올 수 있기 때문에 구분해준 것이다.
+/*
             if(a_count[i] > b_count[i]) // a의 알파벳이 b의 알파벳보다 많은 경우 그 갯수만큼 더해주면 된다.
                 answer += a_count[i] - b_count[i];
             else if(b_count[i] > a_count[i])
                 answer += b_count[i] - a_count[i];
+*/
+
+            // abs : 절대값을 리턴해주는 함수
+            answer += Math.abs(a_count[i] - b_count[i]);
         }
         System.out.println(answer);
     }
