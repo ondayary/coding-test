75dfd55a0228e3ac4b9083b65daf30f729ea4023
이다온 <ondayary@gmail.com>
2023-04-05 13:16:47 +0900

    백준 - 윤년

---
+++ src/baekjoon/step/N2753.java
@@ -0,0 +1,18 @@
+package baekjoon.step;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N2753 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        if (input % 4 == 0) {
+            if (input % 400 == 0) System.out.println("1");
+            else if (input % 100 == 0) System.out.println("0");
+            else System.out.println("1");
+            }
+        else System.out.println("0");
+    }
+}
