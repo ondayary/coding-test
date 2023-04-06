4411b2d6c3933f6bebce0a3f788d53fc878facf3
이다온 <ondayary@gmail.com>
2023-04-06 18:57:11 +0900

    백준 - A+B-3

---
+++ src/baekjoon/step/N10950.java
@@ -0,0 +1,17 @@
+package baekjoon.step;
+
+import java.util.Scanner;
+
+public class N10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // 테스트 케이스 개수를 입력받을 변수
+
+        for (int i = 0; i < T; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A + B);
+        }
+        sc.close();
+    }
+}
