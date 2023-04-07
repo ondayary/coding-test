e6ef7029c3fdea9523d04648bb0f76c812b0aeb8
이다온 <ondayary@gmail.com>
2023-04-07 16:18:22 +0900

    백준 - 별찍기1

---
+++ src/baekjoon/step/N2438.java
@@ -0,0 +1,42 @@
+/*
+백준 - 단계별로 풀어보기 - 반복문 - 별찍기1
+
+문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+
+예제 입력
+5
+
+예제 출력
+*
+**
+***
+****
+*****
+ */
+
+package baekjoon.step;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
